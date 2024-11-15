31496942a964c8831dec69938631f84870965dba
Matthew <recinto.matt@gmail.com>
2024-11-15 08:30:01 -0800

    created website structure

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+
+// import all the components here
+// import Example from './example/example'
+import Navbar from './navbar/navbar'
+
+
+// app logic
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      {/* <Example/> */}
+    
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+
+import './navbar.css';
+//WAIT THIS IS SO COOL - MATTHEW
+function Navbar() {
+    return (<nav>
+               <div class="logo">ohlonehacks</div>
+               <div class="nav-items">
+                   <div class="item">dude</div>
+                   <div class="item">two</div>
+                   <div class="item">three</div>
+               </div>
+           </nav>);
+}
+
+export default Navbar;
