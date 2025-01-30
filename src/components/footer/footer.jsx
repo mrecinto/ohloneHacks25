ae317528bb6706bd6a0cb3689e50a2c78ea86d51
Stemaskk <aidenschung@gmail.com>
2025-01-29 17:04:35 -0800

    V.0.2

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,18 +2,59 @@ import './footer.css';
 function footer() {
 return(
     <footer>
-        <ul>
-            <li>
-                <div class = "footer-items">
-                    <a href = "/">
-                        <div class="item">Home</div>
-                    </a>
-                    <a href="/about">
-                        <div class="item">About</div>
-                    </a>
-                </div>
-            </li>
-        </ul>
+        <div className="footer-items">
+            <a href="/">
+                <div className="item">Home</div>
+            </a>
+            <a href="/about">
+                <div className="item">About</div>
+            </a>
+        </div>
+        {/*<div className="scroll-container">*/}
+        {/*    <div className="scroll-right">*/}
+        {/*        <span className="name">Fatima</span>*/}
+        {/*        <span className="name">Matthew</span>*/}
+        {/*        <span className="name">Aiden</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*    </div>*/}
+        {/*    <div className="scroll-left">*/}
+        {/*        <span className="name">Clement</span>*/}
+        {/*        <span className="name">Tanay</span>*/}
+        {/*        <span className="name">Jazmin</span>*/}
+        {/*        <span className="name">Mama</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*        <span className="name">Name</span>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
+        <div id="animated-text-strip">
+            <span className="marquee">Text length has to be equal&nbsp;</span>
+            <span className="marquee">Text length has to be equal&nbsp;</span>
+            <span className="marquee">Text length has to be equal&nbsp;</span>
+        </div>
     </footer>
 );
 }
