1f89b308bc171647b9b96d4fd828e5630a071392
ibrahimkhadri1 <ibrahimkhadri@outlook.com>
2025-02-07 17:11:11 -0800

    updated slideshow centered

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 
 const images = [
   "/image1.jpg",
@@ -14,24 +13,31 @@ export default function Slideshow() {
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
+    }, 3500);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="slideshow-container">
-      <div className="slideshow">
-        {images.map((src, i) => (
-          <motion.img
-            key={i}
-            src={src}
-            alt={`Slide ${i + 1}`}
-            className="slide"
-            animate={{ x: `${(i - index) * 100}%` }}
-            transition={{ type: "tween", duration: 0.8 }}
-          />
-        ))}
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div className="slideshow-container" style={{ overflow: "hidden", width: "500px", position: "relative" }}>
+        <div
+          className="slideshow"
+          style={{
+            display: "flex",
+            transition: "transform 1s ease-in-out",
+            transform: `translateX(-${index * 100}%)`
+          }}
+        >
+          {images.map((src, i) => (
+            <img
+              key={i}
+              src={src}
+              alt={`Slide ${i + 1}`}
+              style={{ width: "500px", height: "300px", flexShrink: 0 }}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
