299aec78e6b97ab55249eb6b5ef212bb78a138ba
Matthew <recinto.matt@gmail.com>
2025-02-08 16:16:37 -0800

    fixed picture paths issue

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import "./about.css";
-
+import building from "./building.png"; // Correct import for image
+import hill from "./hill.png"; // Correct import for image
 
 const AboutUs = () => {
-  
   return (
     <div className="about-us-container">
       <div className="main-section">
         <div className="image-building">
-          <img src="/src/components/about/building.png" alt="Building" />
+          <img src={building} alt="Building" />
         </div>
         <div className="about-text">
-        <h2>
-        {/* <span className="letter">A</span>
-        <span className="letter">b</span>
-        <span className="letter">o</span>
-        <span className="letter">u</span>
-        <span className="letter">t</span>
-        <span className="letter"> </span>
-        <span className="letter">U</span>
-        <span className="letter">s</span> */}
-        </h2>
+          <h2>
+            {/* If you want to animate the letters individually, uncomment below */}
+            {/* <span className="letter">A</span>
+            <span className="letter">b</span>
+            <span className="letter">o</span>
+            <span className="letter">u</span>
+            <span className="letter">t</span>
+            <span className="letter"> </span>
+            <span className="letter">U</span>
+            <span className="letter">s</span> */}
+          </h2>
           <p>
-          <b class="highlight">OhloneHacks</b> is more than just a tech community - it's the first community college 
-          hackathon in the Bay Area, bringing together over 100 hackers from diverse backgrounds. 
-          We believe in <b class="highlight">empowering </b> students to <b class="highlight"> explore </b> technology, <b class="highlight"> collaborate </b>
-          on impactful projects, and <b class="highlight"> build </b> real solutions while <b class="highlight">networking</b> with industry professionals.
+            <b className="highlight">OhloneHacks</b> is more than just a tech community - it's the first
+            community college hackathon in the Bay Area, bringing together over 100 hackers
+            from diverse backgrounds. We believe in <b className="highlight">empowering</b> students to{" "}
+            <b className="highlight">explore</b> technology, <b className="highlight">collaborate</b> on impactful projects,
+            and <b className="highlight">build</b> real solutions while <b className="highlight">networking</b> with industry professionals.
           </p>
         </div>
       </div>
@@ -40,7 +42,7 @@ const AboutUs = () => {
         </div>
 
         <div className="right-image">
-          <img src="/src/components/about/hill.png" alt="Hill" />
+          <img src={hill} alt="Hill" />
         </div>
       </div>
     </div>
