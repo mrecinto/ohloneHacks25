eb3f2d7bace509da3e42c744964ed1d87c894cb5
Matthew <recinto.matt@gmail.com>
2025-02-08 16:07:53 -0800

    edited some text

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -9,13 +9,14 @@ function FAQ() {
     };
 
     const faqData = [
-        { question: "What is a Hackathon?", answer: "A hackathon is an event where you turn ideas into real projects. It typically lasts 36 hours and is filled with learning and building in a collaborative environment." },
-        { question: "Who can come?", answer: "Anyone passionate about learning and creating can join, regardless of experience." },
+        { question: "What is a Hackathon?", answer: "A hackathon is an event where you turn ideas into real projects. OhloneHacks is a 2-day event filled with learning and building in a collaborative environment." },
+        { question: "Is it overnight?", answer: "Unfortunately not. However, it is a 2-day event where most hacking can be done in-person on Saturday and early morning Sunday. Judging is set for around afternoon on Sunday." },
+        { question: "Who can come?", answer: "Anyone who is passionate and above the age of 18 is invited to sign up!" },
         { question: "What does it cost?", answer: "It’s free! Meals, swag, and other arrangements are all provided." },
-        { question: "What if I don't know how to code?", answer: "This Hackathon will be super beginner-friendly!" },
-        { question: "What if I don't have a team or idea?", answer: "No worries, you can work alone. You can also choose any of our tracks to work on." },
-        { question: "What can I build?", answer: "We will have four different tracks (Track 1, Track 2, Track 3, Track 4). You are free to choose from any!" },
-        { question: "Are there prizes?", answer: "Yes, there are many prizes. TBA at a later date." },
+        { question: "What if I have little to no experience?", answer: "This hackathon will be super beginner-friendly! We will provide support to hackers with mentors and various workshops. In the end, as long as you come out learning something new, that's all that matters!" },
+        { question: "What if I don't have a team?", answer: "You may choose to work alone, however, we will provide a networking section located in both the event space and on our OhloneHacks Discord (that will be launched a week before the event!)" },
+        { question: "What can I build?", answer: "We will have four different tracks that tackle different challenges. You are free to choose from any one of them on the event day!" },
+        { question: "Are there prizes?", answer: "Yes, there are prizes for each track." },
         { question: "How do I apply?", answer: "Fill out the registration form above!" },
     ];
 
@@ -23,7 +24,7 @@ function FAQ() {
         
         <div className="faq-section">
             
-            <h2 className="faq-title">FAQ's</h2>
+            <h2 className="faq-title">FAQs</h2>
             <div className="faq-items">
                 {faqData.map((faq, index) => (
                     <div
